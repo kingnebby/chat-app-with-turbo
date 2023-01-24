02fce16c0851fe4e98ef08aa622f476944965ac1
Dan Villa <danielavilla02@gmail.com>
2023-01-24 11:30:00 -0600

    chore: update terminals and remove prisma schema generator

--- apps/users-api/src/_gen/prisma-class/index.ts
+++
@@ -1,9 +0,0 @@
-import { Profile as _Profile } from './profile';
-import { User as _User } from './user';
-
-export namespace PrismaModel {
-  export class Profile extends _Profile {}
-  export class User extends _User {}
-
-  export const extraModels = [Profile, User];
-}
