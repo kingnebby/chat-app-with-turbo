24a8ea5b892ac02c58afb9cc1e5d62070df04fbb
Dan Villa <danielavilla02@gmail.com>
2023-02-07 09:52:50 -0600

    chore: add token to ui

--- packages/ui/components/UsersList.tsx
+++ packages/ui/components/UsersList.tsx
@@ -5,8 +5,16 @@ export function UsersList() {
   const { isLoading, error, data } = useQuery<UsersDTO[]>({
     queryKey: ['data'],
     queryFn: async () => {
-      const res = await fetch('http://localhost:9001/users', {})
-      return res.json()
+      // TODO: this much better X-D
+      const res = await fetch('http://localhost:9001/users', {
+        headers: {
+          authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtQGplbWluaS5pbyIsInVzZXJuYW1lIjoiam95d2F2ZSIsInN1YiI6Miwicm9sZXMiOlsiQURNSU4iLCJVU0VSIl0sImlhdCI6MTY3NDg0OTE5NywiZXhwIjoxNjc0ODQ5MjU3fQ.2_ctdNSplhQG0kI4okzbKLphrxiQNR6ysUECtm2UXkw'
+        }
+      })
+      if (res.status === 200) {
+        return res.json()
+      }
+      throw new Error('could not fetch users')
     }
   })
 
